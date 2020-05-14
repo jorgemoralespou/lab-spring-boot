bc4931c90f1ec4e309da9f6e1de02ed4f91324a0
BoykoAlex <aboyko@pivotal.io>
2020-05-14 18:44:50 -0400

    Revert previous change to step2.md creation script

--- workshop/setup.d/create-resources.sh
+++ workshop/setup.d/create-resources.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# pwd > /home/eduk8s/create-resources-dir
-# cd /opt/eduk8s
-# envsubst < workshop/content/exercises/step2.md.in > workshop/content/exercises/step2.md
+pwd > /home/eduk8s/create-resources-dir
+cd /opt/eduk8s
+envsubst < workshop/content/exercises/step2.md.in > workshop/content/exercises/step2.md
